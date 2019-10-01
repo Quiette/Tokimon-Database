d7018c44d012c350d55aac86a2449160e82b1f60
Quiette <55117989+Quiette@users.noreply.github.com>
2019-09-30 19:18:57 -0700

    test hello page

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const pool = new Pool({
     ssl: true
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-   .get('/', (req, res) => res.render('pages/index'))
+var app = express();
 
-   .get('/db', async (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+    //is equivalent to .get('/', function (req, res) {res.render('pages/index'})
+app.get('/db', async (req, res) => {
         try {
             const client = await pool.connect()
             const result = await client.query('SELECT * FROM test_table');
@@ -25,6 +26,8 @@ express()
             console.error(err);
             res.send("Error " + err);
         }
-    })
-    //*/
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+})
+
+app.get('/hello', (req, res) => { res.render('pages/hello') });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
